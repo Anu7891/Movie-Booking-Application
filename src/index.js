bfc9c4c64f564f73ff821152e9179a63303bef97
Anubhav Singh <singhanubhav8783@gmail.com>
2022-09-18 23:08:14 +0530

    Checkpoint 3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Home from './screens/home/Home';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './Home.css';
 import Header from '../../common/header/Header';
+import SingleLineGridList from '../../component/SingleLineGridList';
+import Left from '../../component/Left';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <div className='header-home'>
+                    <span >Upcoming Movies</span>
+                </div>
+                <SingleLineGridList />
+                <div className='flex-container'>
+                    <div className='left'>
+                        <Left />
+
+                    </div>
+                    <div className='right'>
+
+                    </div>
+                </div>
+
             </div>
         )
     }
