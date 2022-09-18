ab715de636086dad477f9313ba608eb3e9cd1fe4
Anubhav Singh <singhanubhav8783@gmail.com>
2022-09-18 15:40:08 +0530

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './common/header/Header';
-// import Header from './common/header/Header';
+import Home from './screens/home/Home';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <Home />
   </React.StrictMode>
 );
 
