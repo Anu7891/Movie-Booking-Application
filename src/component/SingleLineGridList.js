6005622978b14c27d2ba16054eb0cdd6604d3ae4
Anubhav Singh <singhanubhav8783@gmail.com>
2022-10-25 13:15:59 +0530

    Created Folder moviebooking

--- src/component/SingleLineGridList.js
+++ src/component/SingleLineGridList.js
@@ -7,35 +7,35 @@ import IconButton from '@mui/material/IconButton';
 import moviesData from '../common/moviesData';
 
 export default function SingleLineGridList() {
-  return (
-    <ImageList sx={{
-      height: 250, gridAutoFlow: 'column', gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))",
-      gridAutoColumns: "minmax(250px,1fr)"
-    }}>
-      <ImageListItem key="Subheader" cols={2}>
-      </ImageListItem>
-      {moviesData.map((item) => (
-        <ImageListItem key={item.id}>
-          <img
-            src={item.poster_url}
-            alt={item.title}
-            loading="lazy"
-            style={{ height: "150px" }}
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${item.title}`}
-              >
-                {/* <InfoIcon /> */}
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
+    return (
+        <ImageList sx={{
+            height: 250, gridAutoFlow: 'column', gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))",
+            gridAutoColumns: "minmax(250px,1fr)"
+        }}>
+            <ImageListItem key="Subheader" cols={2}>
+            </ImageListItem>
+            {moviesData.map((item) => (
+                <ImageListItem key={item.id}>
+                    <img
+                        src={item.poster_url}
+                        alt={item.title}
+                        loading="lazy"
+                        style={{ height: "150px" }}
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={item.author}
+                        actionIcon={
+                            <IconButton
+                                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                aria-label={`info about ${item.title}`}
+                            >
+                                {/* <InfoIcon /> */}
+                            </IconButton>
+                        }
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
 }
